72947d83428e84e4caafa9596c375040b97fcad2
Karmantez <taylous99@gmail.com>
2019-07-14 23:19:44 +0900

    update: setting environment

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function(done) {
+
+    // execute eslint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+
+    // node source
+    gulp.src('es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
+
+    // browser source
+    gulp.src('public/es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"));
+    
+    done();
+});
